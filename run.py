16b536e613bd8f26c694bc32f4edfa8c744e960c
diggzhang <diggzhang@gmail.com>
2017-10-24 15:14:55 +0800

    Change config load method

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from app import app
-from config import *
 
 if __name__ == '__main__':
-    app.run(host=SERVER_ADDR, port=SERVER_PORT, debug=ISDEBUG)
+    app.config.from_object('config')
+    app.run(host=app.config.get('SERVER_ADDR'),
+            port=app.config.get('SERVER_PORT'),
+            debug=app.config.get('ISDEBUG'))
